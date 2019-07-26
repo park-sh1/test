a29525dda16cfde3b5aac79501099e928fb738e0
user1 <user1@user1-PC>
2019-07-26 10:44:08 +0900

    TestVO 미완성상태

---
+++ src/com/kg/test/model/TestVO.java
@@ -0,0 +1,18 @@
+package com.kg.test.model;
+
+public class TestVO {
+	private String name;
+	
+	public TestVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
